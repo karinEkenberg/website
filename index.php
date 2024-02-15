fad41607709c5eb8517b053cbd0407fd411f2d77
karinEkenberg <zirkon6@hotmail.com>
2024-02-15 20:49:52 +0100

    Changed header and footer colors, added user information to the body. (generated by blackbox.ai )

--- index.php
+++ index.php
@@ -11,8 +11,20 @@
         <h1>My first PHP project</h1>
     </header>
     <?php
+        $name = "Karin";
+        $favFood = "hotpot";
+        $age = 35;
+        $pi = 3.14;
+        $price = 2.99;
+        $online = true;
+        
+
         echo "<div class='message'> Welcome to my first PHP project! <br>";
-        echo "<div class='message'> It is a great day to learn PHP!";
+        echo "<div class='message'> It is a great day to learn PHP! <br>";
+        echo "<div class='message'>My name is {$name} and I am {$age} years old, my favorite food is {$favFood}. <br>";
+        echo "<div class='message'>Pi is = {$pi}. <br>";
+        echo "<div class='message'>\${$price} is a normal price for creme fraiche these days...<br>";
+        echo "<div class='message'>Online status: {$online}.<br>";
     ?>
     <br>
     <button class="buttons">Click me!</button><br>
