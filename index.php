5b40937caf5905a9694a00a741bf318e5f35716c
karinEkenberg <zirkon6@hotmail.com>
2024-02-15 15:17:11 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <header>
+        <h1>My first PHP project</h1>
+    </header>
+    <?php
+        echo "<div class='message'> Welcome to my first PHP project! <br>";
+        echo "<div class='message'> It is a great day to learn PHP!";
+    ?>
+    <footer>
+        <p>Created by: Karin Ekenberg</p>
+    </footer>
+</body>
+</html>
